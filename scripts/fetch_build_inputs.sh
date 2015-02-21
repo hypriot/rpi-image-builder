373e3a3ce083d66aa53002b5f3fe2d1383f8f7c9
Stefan Scherer <scherer_stefan@icloud.com>
2015-02-21 20:54:52 +0100

    use relative path both on S3 and in local build_inputs

--- scripts/fetch_build_inputs.sh
+++ scripts/fetch_build_inputs.sh
@@ -3,8 +3,8 @@ RPI_IMAGE_BUILDER_ROOT=${RPI_IMAGE_BUILDER_ROOT:="/vagrant"}
 BUILD_INPUTS=$RPI_IMAGE_BUILDER_ROOT/build_inputs
 
 # provide the name of the packages that should be fetched here
-KERNEL_PACKAGES="kernel/libraspberrypi-bin_20150218-231723_armhf.deb kernel/libraspberrypi-dev_20150218-231723_armhf.deb kernel/libraspberrypi-doc_20150218-231723_armhf.deb kernel/libraspberrypi0_20150218-231723_armhf.deb kernel/raspberrypi-bootloader_20150218-231723_armhf.deb"
-DOCKER_PACKAGES="docker/deb/docker_1.5.0hypriot-1_armhf.deb"
+KERNEL_PACKAGES="kernel/libraspberrypi-bin_20150221-190136_armhf.deb kernel/libraspberrypi-dev_20150221-190136_armhf.deb kernel/libraspberrypi-doc_20150221-190136_armhf.deb kernel/libraspberrypi0_20150221-190136_armhf.deb kernel/raspberrypi-bootloader_20150221-190136_armhf.deb"
+DOCKER_PACKAGES="docker/deb/docker-hypriot_1.5.0-7_armhf.deb"
 PACKAGES="$KERNEL_PACKAGES $DOCKER_PACKAGES"
 
 # ensure that the target directories exist
@@ -14,6 +14,6 @@ mkdir -p $BUILD_INPUTS/docker
 # fetch packages and drop them into our target directory
 for pkg in $PACKAGES; do
   if [ ! -f $BUILD_INPUTS/$pkg ]; then
-    aws s3 --region eu-central-1 cp s3://buildserver-production/$pkg $BUILD_INPUTS/$(basename "$pkg")
+    aws s3 --region eu-central-1 cp s3://buildserver-production/$pkg $BUILD_INPUTS/$pkg
   fi
 done
