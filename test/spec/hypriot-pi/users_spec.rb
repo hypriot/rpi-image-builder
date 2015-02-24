b174366ad25a0690c94329bfeae115e474f6efd9
Stefan Scherer <scherer_stefan@icloud.com>
2015-02-24 21:43:49 +0100

    check group docker and user pi

--- test/spec/hypriot-pi/users_spec.rb
+++ test/spec/hypriot-pi/users_spec.rb
@@ -4,8 +4,13 @@ describe user('root') do
   it { should have_login_shell '/bin/bash' }
 end
 
+describe group('docker') do
+  it { should exist }
+end
+
 describe user('pi') do
   it { should exist }
   it { should have_home_directory '/home/pi' }
   it { should have_login_shell '/bin/bash' }
+  it { should belong_to_group 'docker' }
 end
