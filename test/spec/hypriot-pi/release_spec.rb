97424e4f1f1e96b73629da4dd7fc34724a200995
Stefan Scherer <scherer_stefan@icloud.com>
2015-02-26 01:16:13 +0100

    test that /etc/hypriot_release file has commit infos

--- test/spec/hypriot-pi/release_spec.rb
+++ test/spec/hypriot-pi/release_spec.rb
@@ -2,4 +2,7 @@ describe file('/etc/hypriot_release') do
   it { should be_file }
   its(:content) { should match /profile: hypriot/ }
   its(:content) { should match /build: \d+\-\d+/ }
+  its(:content) { should match /commit: [0-9a-f]+/ }
+  its(:content) { should match /kernel_build: \d+\-\d+/ }
+  its(:content) { should match /kernel_commit: [0-9a-f]+/ }
 end
