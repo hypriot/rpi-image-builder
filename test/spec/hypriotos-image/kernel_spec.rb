396a117fe944267ef2844ac330e4f9e3343902ac
Dieter Reuter <dieter.reuter@me.com>
2015-03-01 12:38:22 +0100

    merge changes from master

--- test/spec/hypriotos-image/kernel_spec.rb
+++ test/spec/hypriotos-image/kernel_spec.rb
@@ -3,24 +3,20 @@ describe command('uname -r') do
   its(:exit_status) { should eq 0 }
 end
 
-describe file('/lib/modules/3.18.7+/build') do
-  it { should_not be_file }
-  it { should_not be_symlink }
+describe file('/lib/modules/3.18.7+') do
+  it { should_not be_directory }
 end
 
-describe file('/lib/modules/3.18.7-v7+/build') do
-  it { should_not be_file }
-  it { should_not be_symlink }
+describe file('/lib/modules/3.18.7-v7+') do
+  it { should_not be_directory }
 end
 
-describe file('/lib/modules/3.18.8+/build') do
-  it { should_not be_file }
-  it { should_not be_symlink }
+describe file('/lib/modules/3.18.8+') do
+  it { should_not be_directory }
 end
 
-describe file('/lib/modules/3.18.8-v7+/build') do
-  it { should_not be_file }
-  it { should_not be_symlink }
+describe file('/lib/modules/3.18.8-v7+') do
+  it { should_not be_directory }
 end
 
 Specinfra::Runner.run_command('modprobe btrfs')
@@ -39,4 +35,23 @@ describe file('/boot/cmdline.txt') do
   its(:content) { should match /rootfstype=ext4/ }
   its(:content) { should match /cgroup-enable=memory/ }
   its(:content) { should match /swapaccount=1/ }
+end
+
+# with installed kernel headers
+describe file('/lib/modules/3.18.8-hypriotos+/build') do
+  it { should be_symlink }
+  it { should be_linked_to '/usr/src/linux-headers-3.18.8-hypriotos+' }
+end
+
+describe file('/lib/modules/3.18.8-hypriotos-v7+/build') do
+  it { should be_symlink }
+  it { should be_linked_to '/usr/src/linux-headers-3.18.8-hypriotos-v7+' }
+end
+
+describe file('/usr/src/linux-headers-3.18.8-hypriotos-v7+') do
+  it { should be_directory }
+end
+
+describe file('/usr/src/linux-headers-3.18.8-hypriotos+') do
+  it { should be_directory }
 end
