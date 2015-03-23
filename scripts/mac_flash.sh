560f4d5721541c43dada346fbb5aa5a2fa8a35c7
Stefan Scherer <scherer_stefan@icloud.com>
2015-03-23 14:36:08 +0100

    flash.sh from http and s3 links

--- scripts/mac_flash.sh
+++ scripts/mac_flash.sh
@@ -5,6 +5,9 @@ function usage {
   exit 1
 }
 
+beginswith() { case $2 in $1*) true;; *) false;; esac; }
+endswith() { case $2 in *$1) true;; *) false;; esac; }
+
 if [ "$1" == "" ]; then
   usage
 fi
@@ -12,9 +15,31 @@ if [ "$1" == "--help" ]; then
   usage
 fi
 
-if [ ! -f "$1" ]; then
+image=$1
+
+if beginswith http:// "$image"; then
+  echo "Downloading $image ..."
+  curl -L -o /tmp/image.img.zip "$image"
+  image=/tmp/image.img.zip
+fi
+
+if beginswith s3:// "$image"; then
+  echo "Downloading $image ..."
+  aws s3 cp "$image" /tmp/image.img.zip
+  image=/tmp/image.img.zip
+fi
+
+if [ ! -f "$image" ]; then
   echo "File not found."
   exit 10
+fi
+
+if endswith .zip "$image"; then
+  echo "Uncompressing $image ..."
+  unzip -o "$image" -d /tmp
+  image=$(unzip -l "$image" | grep -v Archive: | grep img | cut -c 30-)
+  image="/tmp/$image"
+  echo "Use $image"
 fi
 
 if [[ "$OSTYPE" != "darwin"*  ]]; then
@@ -46,15 +71,15 @@ done
 echo "Unmounting ${disk} ..."
 diskutil unmountDisk /dev/${disk}s1
 diskutil unmountDisk /dev/${disk}
-echo "Flashing $1 to ${disk} ..."
+echo "Flashing $image to ${disk} ..."
 pv=`which pv 2>/dev/null`
 if [ $? -eq 0 ]; then
-  size=`stat -f %z $1`
-  cat $1 | pv -s $size | sudo dd bs=1m of=/dev/r${disk}
+  size=`stat -f %z $image`
+  cat $image | pv -s $size | sudo dd bs=1m of=/dev/r${disk}
 else
   echo "No `pv` command found, so no progress available."
   echo "Press CTRL+T if you want to see the current info of dd command."
-  sudo dd bs=1m if=$1 of=/dev/r${disk}
+  sudo dd bs=1m if=$image of=/dev/r${disk}
 fi
 echo "Unmounting and ejecting ${disk} ..."
 sleep 1
