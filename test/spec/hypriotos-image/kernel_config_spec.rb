d96dc250730871c00a4a6eea7bbe6ec0d62f7caf
Dieter Reuter <dieter.reuter@me.com>
2015-04-12 20:23:23 +0200

    Add tests for kernel modules for kernel tracing

---
+++ test/spec/hypriotos-image/kernel_config_spec.rb
@@ -0,0 +1,14 @@
+describe command('zcat /proc/config.gz') do
+  its(:stdout) { should match /CONFIG_KPROBES=y/ }
+  its(:stdout) { should match /CONFIG_UPROBES=y/ }
+  its(:stdout) { should match /CONFIG_HAVE_KPROBES=y/ }
+  its(:stdout) { should match /CONFIG_EVENT_TRACING=y/ }
+  its(:stdout) { should match /CONFIG_KPROBE_EVENT=y/ }
+  its(:stdout) { should match /CONFIG_UPROBE_EVENT=y/ }
+  its(:stdout) { should match /CONFIG_PROBE_EVENTS=y/ }
+  its(:stdout) { should match /CONFIG_FTRACE=y/ }
+  its(:stdout) { should match /CONFIG_FTRACE_SYSCALLS=y/ }
+  its(:stdout) { should match /CONFIG_DYNAMIC_FTRACE=y/ }
+  its(:stdout) { should match /CONFIG_HAVE_DYNAMIC_FTRACE=y/ }
+  its(:exit_status) { should eq 0 }
+end
