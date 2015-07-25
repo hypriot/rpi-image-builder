bfc6c3c7adac7175ee615f282f790eca959a72f2
Stefan Scherer <scherer_stefan@icloud.com>
2015-07-25 12:54:19 +0200

    resize rootfs without reboot

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -377,16 +377,63 @@ dpkg-reconfigure -f noninteractive tzdata
 
 echo 'Reconfigured timezone' >> /dev/kmsg
 
+do_expand_rootfs() {
+  if ! [ -h /dev/disk/by-label/root ]; then
+    whiptail --msgbox \"/dev/disk/by-label/root does not exist or is not a symlink. Don't know how to expand\" 20 60 2
+    return 0
+  fi
+
+  ROOT_PART=$(readlink /dev/disk/by-label/root)
+  PART_NUM=${ROOT_PART#../../mmcblk0p}
+  if [ \"$PART_NUM\" = \"$ROOT_PART\" ]; then
+    whiptail --msgbox \"/dev/disk/by-label/root is not an SD card. Don't know how to expand\" 20 60 2
+    return 0
+  fi
+
+  # NOTE: the NOOBS partition layout confuses parted. For now, let's only
+  # agree to work with a sufficiently simple partition layout
+  if [ \"$PART_NUM\" -ne 2 ]; then
+    whiptail --msgbox \"Your partition layout is not currently supported by this tool. You are probably using NOOBS, in which case your root filesystem is already expanded anyway.\" 20 60 2
+    return 0
+  fi
+
+  LAST_PART_NUM=$(parted /dev/mmcblk0 -ms unit s p | tail -n 1 | cut -f 1 -d:)
+
+  if [ \"$LAST_PART_NUM\" != \"$PART_NUM\" ]; then
+    whiptail --msgbox \"/dev/disk/by-label/root is not the last partition. Don't know how to expand\" 20 60 2
+    return 0
+  fi
+
+  # Get the starting offset of the root partition
+  PART_START=$(parted /dev/mmcblk0 -ms unit s p | grep \"^${PART_NUM}\" | cut -f 2 -d: | cut -f 1 -ds)
+  [ \"$PART_START\" ] || return 1
+  # Return value will likely be error for fdisk as it fails to reload the
+  # partition table because the root fs is mounted
+  fdisk /dev/mmcblk0 <<EOF
+p
+d
+$PART_NUM
+n
+p
+$PART_NUM
+$PART_START
+
+p
+w
+EOF
+
+  partprobe
+  resize2fs /dev/disk/by-label/root
+}
 
 # Expand filesystem, but only on real device, not in QEMU
 if [ ! -e /dev/sda ]; then
   echo 'Expanding rootfs ...' >> /dev/kmsg
-  echo 'DUMMY! NOTHING WILL BE CHANGED BY THIS SCRIPT.'
-  # raspi-config --expand-rootfs
+  do_expand_rootfs()
+
   echo 'Expand rootfs done' >> /dev/kmsg
 
-  # sudo systemctl enable raspi-config-resize2fs.service
-  # sudo systemctl enable docker.service
+  sudo systemctl enable docker.service
 
   # sleep 5
   # reboot
