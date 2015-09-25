a63fa67a720c518c940f0980043d1c12d424fea6
Stefan Scherer <scherer_stefan@icloud.com>
2015-09-26 00:15:29 +0200

    Add test for docker-compose

---
+++ test/spec/hypriotos-image/docker-compose_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe package('docker-compose') do
+  it { should be_installed }
+end
+
+describe command('dpkg -l docker-compose') do
+  its(:stdout) { should match /ii  docker-compose/ }
+  its(:stdout) { should match /1.4.2-61/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe file('/usr/local/bin/docker-compose') do
+  it { should be_file }
+  it { should be_mode 755 }
+  it { should be_owned_by 'root' }
+end
