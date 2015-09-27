f82b4ec45b54ffbd643bdc9315d927766444250e
firecyberice <andreas@hypriot.com>
2015-09-27 22:02:08 +0200

    Update fetch_build_inputs.sh

--- scripts/fetch_build_inputs.sh
+++ scripts/fetch_build_inputs.sh
@@ -31,4 +31,4 @@ for pkg in $PACKAGES; do
   fi
 done
 
-aws s3 --region eu-central-1 cp s3://buildserver-production/docker-images/rpi-swarm/2015-09-27_4759e5a/rpi-swarm.tar $BUILD_INPUTS/docker/swarm.tar.gz
+aws s3 --region eu-central-1 cp s3://buildserver-production/docker-images/rpi-swarm/2015-09-27_de03978/rpi-swarm.tar $BUILD_INPUTS/docker/swarm.tar.gz
