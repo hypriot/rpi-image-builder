5524f1bd4b065f5027b7c84662d49675c40a9764
Dieter Reuter <dieter.reuter@me.com>
2015-11-08 16:58:29 +0100

    Change swarm version to 1.0.0

--- test/spec/hypriotos-docker/docker-swarm_spec.rb
+++ test/spec/hypriotos-docker/docker-swarm_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe command('docker images hypriot/rpi-swarm') do
-  its(:stdout) { should match /hypriot\/rpi-swarm .*latest .*39e43f2161c9 / }
+  its(:stdout) { should match /hypriot\/rpi-swarm .*latest .*039c550f6208 / }
   its(:exit_status) { should eq 0 }
 end
 
@@ -10,6 +10,6 @@ describe file('/var/hypriot/swarm.tar.gz') do
 end
 
 describe command('docker run --rm -t hypriot/rpi-swarm --version') do
-  its(:stdout) { should match /swarm version 0.4.0 \(HEAD\)/ }
+  its(:stdout) { should match /swarm version 1.0.0 \(HEAD\)/ }
   its(:exit_status) { should eq 0 }
 end
