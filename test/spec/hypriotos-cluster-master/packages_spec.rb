73aea9e156cb47330e2ae54139f05a406cbfd58d
Stefan Scherer <scherer_stefan@icloud.com>
2016-02-03 23:05:47 +0100

    Test for hypriot-cluster-lab version 0.1.1

--- test/spec/hypriotos-cluster-master/packages_spec.rb
+++ test/spec/hypriotos-cluster-master/packages_spec.rb
@@ -11,3 +11,11 @@ end
 describe package('dnsmasq') do
   it { should be_installed }
 end
+
+describe package('hypriot-cluster-lab') do
+  it { should be_installed }
+end
+
+describe command("dpkg -l hypriot-cluster-lab") do
+  its(:stdout) { should contain(" 0.1.1-") }
+end
