13fd2ae6c53fbbd65a99accbf56c552fe1c4981b
Dieter Reuter <dieter.reuter@me.com>
2016-03-06 18:22:40 +0100

    Add test for RPi 3 devicetree file

--- test/spec/hypriotos-image/base/bootfiles_spec.rb
+++ test/spec/hypriotos-image/base/bootfiles_spec.rb
@@ -42,6 +42,12 @@ describe file('/boot/bcm2709-rpi-2-b.dtb') do
   it { should be_owned_by 'root' }
 end
 
+describe file('/boot/bcm2710-rpi-3-b.dtb') do
+  it { should be_file }
+  it { should be_mode 755 }
+  it { should be_owned_by 'root' }
+end
+
 describe file('/boot/cmdline.txt') do
   it { should be_file }
   it { should be_mode 755 }
